caae93a5f3ca329f7ff49a643ab8339b1533229b
hungngo97 <hung.ngovinh97@gmail.com>
2020-02-22 15:00:53 -0800

    refactored

--- Perceptron.py
+++ Perceptron.py
@@ -6,73 +6,62 @@ import pickle
 import re
 from models.FeatureGenerator import ( FeatureGenerator )
 from utils.math_utils import *
+
 import Viterbi
 
 featureGenerator = FeatureGenerator()
-def train(labels, examples, stepsize=1, numpasses=10, do_averaging=False, devdata=None):
+def train(labels, data, iterations):
     OUTPUT_VOCAB = labels
     weights = defaultdict(float)
+    cum_weight = defaultdict(float)
 
     S = defaultdict(float)
     t = 0
 
     def get_averaged_weights():
+        # return defaultdict(float,{feature: val / t for feature, val in cum_weight.items()} )
         return defaultdict(float, {feature: weights[feature] - ((1 / t) * val) for feature, val in S.items()})
 
-    for pass_iteration in range(numpasses):
-        print ("Training iteration %d" % pass_iteration)
-
-        for tokens,goldlabels in examples:
-            g = dict_subtract(features_for_seq(tokens, goldlabels), features_for_seq(tokens, predict_seq(tokens, weights, OUTPUT_VOCAB)))
-            for feature, gval in g.items():
-                weights[feature] = weights[feature] + (stepsize * gval)
-                S[feature] = S[feature] + ((t - 1) * stepsize * gval)
+    for i in range(iterations):
+        print ("Training iteration %d" % i)
+        for texts,label in data:
+            diff = dict_subtract(generate_global_feature(texts, label), generate_global_feature(texts, predict_seq(texts, weights, OUTPUT_VOCAB)))
+            for feature, val in diff.items():
+                weights[feature] = weights[feature] + (val)
+                S[feature] = S[feature] + ((t - 1) * val)
+                cum_weight[feature] += weights[feature]
             t += 1
 
-    return weights if not do_averaging else get_averaged_weights()
+    return get_averaged_weights()
 
-def predict_seq(tokens, weights, OUTPUT_VOCAB):
-    if len(tokens) == 0:
+def predict_seq(texts, weights, OUTPUT_VOCAB):
+    if len(texts) == 0:
         return []
-    Ascores, Bscores = calc_factor_scores(tokens, weights, OUTPUT_VOCAB)
+    Ascores, Bscores = calc_feature_weight_value(texts, weights, OUTPUT_VOCAB)
     return Viterbi.viterbi(Ascores, Bscores, OUTPUT_VOCAB)
 
-def greedy_decode(Ascores, Bscores, OUTPUT_VOCAB):
-    N=len(Bscores)
-    if N==0: return []
-    out = [None]*N
-    out[0] = dict_argmax(Bscores[0])
-    for t in range(1,N):
-        tagscores = {tag: Bscores[t][tag] + Ascores[out[t-1], tag] for tag in OUTPUT_VOCAB}
-        besttag = dict_argmax(tagscores)
-        out[t] = besttag
-    return out
-
-def local_emission_features(t, tag, tokens):
+def generate_local_feature(t, tag, texts):
     features = {}
-    features = featureGenerator.generate_local_feature(tokens, t, tag)
+    features = featureGenerator.generate_local_feature(texts, t, tag)
     return features
 
-def features_for_seq(tokens, labelseq):
+def generate_global_feature(texts, labels):
     total = defaultdict(float)
-    for i in range(0, len(tokens)):
-        for feat_name, feat_value in local_emission_features(i, labelseq[i], tokens).items():
+    for i in range(0, len(texts)):
+        for feat_name, feat_value in generate_local_feature(i, labels[i], texts).items():
             total[feat_name] += feat_value
-        """
         if(i > 0):
-            total["trans_%s_%s"% (labelseq[i - 1], labelseq[i])] += 1
-        """
+            total["trans_%s_%s"% (labels[i - 1], labels[i])] += 1
     return total
 
-def calc_factor_scores(tokens, weights, OUTPUT_VOCAB):
-    N = len(tokens)
+def calc_feature_weight_value(texts, weights, OUTPUT_VOCAB):
+    N = len(texts)
     
     Ascores = {} 
-    # Ascores = { (tag1,tag2): weights["trans_%s_%s"% (tag1, tag2)] for tag1 in OUTPUT_VOCAB for tag2 in OUTPUT_VOCAB }
+    Ascores = { (tag1,tag2): weights["trans_%s_%s"% (tag1, tag2)] for tag1 in OUTPUT_VOCAB for tag2 in OUTPUT_VOCAB }
     Bscores = []
     for t in range(N):
         Bscores += [defaultdict(float)]
         for tag in OUTPUT_VOCAB:
-            Bscores[t][tag] += dict_dotprod(weights, local_emission_features(t, tag, tokens))
-    assert len(Bscores) == N
+            Bscores[t][tag] += dict_dotprod(weights, generate_local_feature(t, tag, texts))
     return Ascores, Bscores
--- Viterbi.py
+++ Viterbi.py
@@ -2,25 +2,22 @@ from __future__ import division
 from numpy.random import random
 from utils.math_utils import * 
 
-def viterbi(A_factor, B_factors, output_vocab):
-
-    N = len(B_factors)   # length of input sentence
-    from utils.math_utils import dict_argmax
-
-
-    # viterbi log-prob tables
+def viterbi(tag_transition_features, feature_values, output_vocab):
+    N = len(feature_values)  
+    
+    # backpointer and value table represents all possible tags for each word position
+    # Representation as a table of shape [Labels, Sentence_length] with number 
+    # of rows is total possible labels and columns are each position in the sentence
     V = [{tag:None for tag in output_vocab} for t in range(N)]
-    # backpointer tables
-    # back[0] could be left empty. it will never be used.
     back = [{tag:None for tag in output_vocab} for t in range(N)]
 
-    V[0] = {k: B_factors[0][k] for k, v in V[0].items()}
+    # Base case when it is at the first word, just weight * features
+    V[0] = {k: feature_values[0][k] for k, v in V[0].items()}
 
     for i in range(1, len(V)):
         for k, v in V[i].items():
-            probs = {key: B_factors[i][k] + V[i - 1][key] for key in V[i - 1].keys()}
-            
-            # probs = {key: A_factor[(key, k)] + B_factors[i][k] + V[i - 1][key] for key in V[i - 1].keys()}
+            probs = {key: feature_values[i][k] + V[i - 1][key] for key in V[i - 1].keys()}
+            # probs = {key: tag_transition_features[(key, k)] + feature_values[i][k] + V[i - 1][key] for key in V[i - 1].keys()}
             V[i][k] = max(probs.values())
             back[i][k] = dict_argmax(probs)
 
@@ -28,5 +25,4 @@ def viterbi(A_factor, B_factors, output_vocab):
     trace = [dict_argmax(V[len(V) - 1])]
     for i in range(1, len(back)):
         trace = [back[len(back) - i][trace[0]]] + trace
-
     return trace
